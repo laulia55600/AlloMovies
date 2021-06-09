94ab047e8e7974f08e6e86b1ad3d15955bbdad55
Laulia <saclaurence.dev@gmail.com>
2021-06-09 12:57:22 +0200

    mise en place de la structure Allo Movies

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import {Header, MovieList, MovieDetails} from './components'
+
+function App() {
+  return (
+    <div className="App">
+        <Header />
+        <div className='d-flex flex-row'>
+          <MovieList />
+          <MovieDetails />
+        </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/movie-list/MovieList.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class MovieList extends Component {
+
+  render() {
+    return <div className='w-75'>Movie List</div>
+  }
+
+}
