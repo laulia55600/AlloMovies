7170c350754bc13b20071d571a74623f9d8db719
Laulia <saclaurence.dev@gmail.com>
2021-06-09 13:21:09 +0200

    mise en place du compsant MovieList

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import {Header, MovieList, MovieDetails} from './components'
 
 function App() {
   return (
-    <div className="App">
+    <div className="App d-flex flex-column">
         <Header />
-        <div className='d-flex flex-row'>
+        <div className='d-flex flex-row flex-fill pt-4 p-2'>
           <MovieList />
           <MovieDetails />
         </div>
