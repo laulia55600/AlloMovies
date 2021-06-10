8b969aa3b44b07319e7c0e135fd12bc41a923a07
Laulia <saclaurence.dev@gmail.com>
2021-06-10 14:24:28 +0200

    Simulation d'un temps de chargement et d'un appel api

---
+++ src/components/utils/Loading.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default () => {
+    return (
+        <div style={ { minHeight: '100vh '} } className="d-flex flex-row justify-content-center align-items-center w-100">
+            <img alt="loading gif" src="https://img.pikbest.com/58pic/35/39/61/62K58PICb88i68HEwVnm5_PIC2018.gif!w340" />
+        </div>
+    );
+}
